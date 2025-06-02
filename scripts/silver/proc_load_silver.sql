44f1a32757111c0ea80df16dae8983b2b1209254
YashviSekhani <yashvisekhani18@gmail.com>
2025-06-02 23:29:46 +0530

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -30,6 +30,8 @@ BEGIN
 		PRINT 'Loading CRM Tables';
 		PRINT '--------------------------------------------';
 
+		-- Loading silver.crm_cust_info
+
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating Table : silver.crm_cust_info';
 		TRUNCATE TABLE silver.crm_cust_info;
@@ -51,11 +53,11 @@ BEGIN
 		CASE WHEN UPPER(TRIM(cst_material_status)) = 'S' THEN 'Single'
 			 WHEN UPPER(TRIM(cst_material_status)) = 'M' THEN 'Married'
 			 ELSE 'n/a'
-		END cst_material_status,
+		END cst_material_status, -- Normalize marital status values to readable format
 		CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
 			 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
 			 ELSE 'n/a'
-		END cst_gndr,
+		END cst_gndr, -- Normalize gender values to readable format
 		cst_create_date
 		FROM (
 			SELECT 
@@ -63,11 +65,12 @@ BEGIN
 			ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
 			FROM bronze.crm_cust_info
 			WHERE cst_id IS NOT NULL
-		)t WHERE flag_last = 1;
+		)t WHERE flag_last = 1; -- Select the most recent record per customer
 		SET @end_time = GETDATE();
 		PRINT 'Load Duration: ' + CAST(DATEDIFF(SECOND , @start_time , @end_time) AS NVARCHAR) + 'seconds';
 		PRINT '>> ------------------';
 
+		-- Loading silver.crm_prd_info
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating Table : silver.crm_prd_info';
 		TRUNCATE TABLE silver.crm_prd_info;
@@ -84,8 +87,8 @@ BEGIN
 		)
 		SELECT 
 		prd_id,
-		REPLACE(SUBSTRING(prd_key, 1, 5), '-' , '_') AS cat_id,
-		SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+		REPLACE(SUBSTRING(prd_key, 1, 5), '-' , '_') AS cat_id, -- Extract category ID
+		SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,		-- Extract product key
 		prd_nm,
 		ISNULL(prd_cost, 0) AS prd_cost,
 		CASE UPPER(TRIM(prd_line))
@@ -94,14 +97,15 @@ BEGIN
 			 WHEN 'S' THEN 'Other Sales'
 			 WHEN 'T' THEN 'Touring'
 			 ELSE 'n/a'
-		END AS prd_line,
+		END AS prd_line, -- Map product line codes to descriptive values
 		CAST(prd_start_dt AS DATE) AS prd_start_dt,
-		CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE) AS prd_end_dt
+		CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE) AS prd_end_dt -- Calculate end date as one day before the next start date
 		FROM bronze.crm_prd_info;
 		SET @end_time = GETDATE();
 		PRINT 'Load Duration: ' + CAST(DATEDIFF(SECOND , @start_time , @end_time) AS NVARCHAR) + 'seconds';
 		PRINT '>> ------------------';
 
+		-- Loading crm_sales_details
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating Table : silver.crm_sales_details';
 		TRUNCATE TABLE silver.crm_sales_details;
@@ -133,12 +137,12 @@ BEGIN
 		CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
 			 THEN sls_quantity * ABS(sls_price)
 			 ELSE sls_sales
-		END AS sls_sales,
+		END AS sls_sales, -- Recalculate sales if original value is missing or incorrect
 		sls_quantity,
 		CASE WHEN sls_price IS NULL OR sls_price <= 0 
 			 THEN sls_sales / NULLIF(sls_quantity , 0)
 			 ELSE sls_price
-		END AS sls_price
+		END AS sls_price -- Derive price if original value is invalid
 		FROM bronze.crm_sales_details;
 		SET @end_time = GETDATE();
 		PRINT 'Load Duration: ' + CAST(DATEDIFF(SECOND , @start_time , @end_time) AS NVARCHAR) + 'seconds';
@@ -148,6 +152,7 @@ BEGIN
 		PRINT 'Loading ERP Tables';
 		PRINT '--------------------------------------------';
 
+        	-- Loading erp_cust_az12
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating Table : silver.erp_cust_az12';
 		TRUNCATE TABLE silver.erp_cust_az12;
@@ -158,21 +163,22 @@ BEGIN
 			gen
 		)
 		SELECT 
-		CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4 , LEN(cid))
+		CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4 , LEN(cid)) -- Remove 'NAS' prefix if present
 			 ELSE cid
 		END cid,
 		CASE WHEN bdate > GETDATE() THEN NULL
 			 ELSE bdate
-		END AS bdate,
+		END AS bdate, -- Set future birthdates to NULL
 		CASE WHEN UPPER(TRIM(gen)) IN ('F' , 'FEMALE') THEN 'Female'
 			 WHEN UPPER(TRIM(gen)) IN ('M' , 'MALE') THEN 'Male'
 			 ELSE 'n/a'
-		END AS gen
+		END AS gen -- Normalize gender values and handle unknown cases
 		FROM bronze.erp_cust_az12;
 		SET @end_time = GETDATE();
 		PRINT 'Load Duration: ' + CAST(DATEDIFF(SECOND , @start_time , @end_time) AS NVARCHAR) + 'seconds';
 		PRINT '>> ------------------';
 
+		-- Loading erp_loc_a101
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating Table : silver.erp_loc_a101';
 		TRUNCATE TABLE silver.erp_loc_a101;
@@ -187,12 +193,13 @@ BEGIN
 			 WHEN TRIM(cntry) IN ('US' , 'USA') THEN 'United States'
 			 WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
 			 ELSE TRIM(cntry)
-		END AS cntry
+		END AS cntry -- Normalize and Handle missing or blank country codes
 		FROM bronze.erp_loc_a101;
 		SET @end_time = GETDATE();
 		PRINT 'Load Duration: ' + CAST(DATEDIFF(SECOND , @start_time , @end_time) AS NVARCHAR) + 'seconds';
 		PRINT '>> ------------------';
 
+		-- Loading erp_px_cat_g1v2
 		SET @start_time = GETDATE();
 		PRINT '>> Truncating Table : silver.erp_px_cat_g1v2';
 		TRUNCATE TABLE silver.erp_px_cat_g1v2;

abb4c2f7d9e04bcbefb2c81c86ed1473a50fdd65
Yashvi Sekhani <yashvisekhani18@gmail.com>
2026-04-26 02:03:10 +0530

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -53,11 +53,11 @@ BEGIN
 		CASE WHEN UPPER(TRIM(cst_material_status)) = 'S' THEN 'Single'
 			 WHEN UPPER(TRIM(cst_material_status)) = 'M' THEN 'Married'
 			 ELSE 'n/a'
-		END cst_material_status, -- Normalize marital status values to readable format
+		END cst_material_status, 
 		CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
 			 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
 			 ELSE 'n/a'
-		END cst_gndr, -- Normalize gender values to readable format
+		END cst_gndr, 
 		cst_create_date
 		FROM (
 			SELECT 
@@ -65,7 +65,7 @@ BEGIN
 			ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) as flag_last
 			FROM bronze.crm_cust_info
 			WHERE cst_id IS NOT NULL
-		)t WHERE flag_last = 1; -- Select the most recent record per customer
+		)t WHERE flag_last = 1; 
 		SET @end_time = GETDATE();
 		PRINT 'Load Duration: ' + CAST(DATEDIFF(SECOND , @start_time , @end_time) AS NVARCHAR) + 'seconds';
 		PRINT '>> ------------------';
@@ -87,8 +87,8 @@ BEGIN
 		)
 		SELECT 
 		prd_id,
-		REPLACE(SUBSTRING(prd_key, 1, 5), '-' , '_') AS cat_id, -- Extract category ID
-		SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,		-- Extract product key
+		REPLACE(SUBSTRING(prd_key, 1, 5), '-' , '_') AS cat_id, 
+		SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,		
 		prd_nm,
 		ISNULL(prd_cost, 0) AS prd_cost,
 		CASE UPPER(TRIM(prd_line))
@@ -97,9 +97,9 @@ BEGIN
 			 WHEN 'S' THEN 'Other Sales'
 			 WHEN 'T' THEN 'Touring'
 			 ELSE 'n/a'
-		END AS prd_line, -- Map product line codes to descriptive values
+		END AS prd_line, 
 		CAST(prd_start_dt AS DATE) AS prd_start_dt,
-		CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE) AS prd_end_dt -- Calculate end date as one day before the next start date
+		CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE) AS prd_end_dt 
 		FROM bronze.crm_prd_info;
 		SET @end_time = GETDATE();
 		PRINT 'Load Duration: ' + CAST(DATEDIFF(SECOND , @start_time , @end_time) AS NVARCHAR) + 'seconds';
@@ -137,12 +137,12 @@ BEGIN
 		CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
 			 THEN sls_quantity * ABS(sls_price)
 			 ELSE sls_sales
-		END AS sls_sales, -- Recalculate sales if original value is missing or incorrect
+		END AS sls_sales, 
 		sls_quantity,
 		CASE WHEN sls_price IS NULL OR sls_price <= 0 
 			 THEN sls_sales / NULLIF(sls_quantity , 0)
 			 ELSE sls_price
-		END AS sls_price -- Derive price if original value is invalid
+		END AS sls_price 
 		FROM bronze.crm_sales_details;
 		SET @end_time = GETDATE();
 		PRINT 'Load Duration: ' + CAST(DATEDIFF(SECOND , @start_time , @end_time) AS NVARCHAR) + 'seconds';
@@ -163,16 +163,16 @@ BEGIN
 			gen
 		)
 		SELECT 
-		CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4 , LEN(cid)) -- Remove 'NAS' prefix if present
+		CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4 , LEN(cid)) 
 			 ELSE cid
 		END cid,
 		CASE WHEN bdate > GETDATE() THEN NULL
 			 ELSE bdate
-		END AS bdate, -- Set future birthdates to NULL
+		END AS bdate, 
 		CASE WHEN UPPER(TRIM(gen)) IN ('F' , 'FEMALE') THEN 'Female'
 			 WHEN UPPER(TRIM(gen)) IN ('M' , 'MALE') THEN 'Male'
 			 ELSE 'n/a'
-		END AS gen -- Normalize gender values and handle unknown cases
+		END AS gen 
 		FROM bronze.erp_cust_az12;
 		SET @end_time = GETDATE();
 		PRINT 'Load Duration: ' + CAST(DATEDIFF(SECOND , @start_time , @end_time) AS NVARCHAR) + 'seconds';
@@ -193,7 +193,7 @@ BEGIN
 			 WHEN TRIM(cntry) IN ('US' , 'USA') THEN 'United States'
 			 WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
 			 ELSE TRIM(cntry)
-		END AS cntry -- Normalize and Handle missing or blank country codes
+		END AS cntry 
 		FROM bronze.erp_loc_a101;
 		SET @end_time = GETDATE();
 		PRINT 'Load Duration: ' + CAST(DATEDIFF(SECOND , @start_time , @end_time) AS NVARCHAR) + 'seconds';
